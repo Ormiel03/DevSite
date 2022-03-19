75cf96808f789b52f4539c2f0c704c6bc251f791
ormiel <ormiel@gmail.com>
2022-03-19 03:39:19 -0700

    Adding application to INSTALLED_APPS Create forms.py, create class LoginForm.

--- DevSite/DevSite/settings.py
+++ DevSite/DevSite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'account_manager.apps.AccountManagerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
