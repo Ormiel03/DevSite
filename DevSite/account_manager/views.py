8997c29e19f71863f27d2cd3fb8f37ca57867e23
ormiel <ormiel@gmail.com>
2022-03-29 03:53:20 -0700

    Add User registration form Add registration link Create and write register.html and register_done.html template Add path to register template add register view

--- DevSite/account_manager/views.py
+++ DevSite/account_manager/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm
-from  django.contrib.auth.decorators import login_required
+from .forms import LoginForm, UserRegistrationForm
+from django.contrib.auth.decorators import login_required
 
-@login_required # sprawdza czy uytkownik jest uwieytelniony, jeeli tak nastepuje udekorowanie.
+
+@login_required  # sprawdza czy uytkownik jest uwieytelniony, jeeli tak nastepuje udekorowanie.
 def dashboard(request):
     return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+
 
 def user_login(request):
     if request.method == 'POST':
@@ -26,3 +28,16 @@ def user_login(request):
         form = LoginForm()
     return render(request, 'account/login.html', {'form': form})
 
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            return render(request, 'account/register_done.html', {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+
+    return render(request, 'account/register.html', {'user_form': user_form})
